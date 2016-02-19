fc8bb72b5380083287056896a957e3fb8e280c92
David Cavar <wizzard405@gmail.com>
2016-02-19 13:14:38 +0100

    initial commit

---
+++ content.js
@@ -0,0 +1,30 @@
+var wtf = document.querySelector('.dashboard-left');
+if(wtf != null && wtf != undefined) {
+    wtf.style.display = 'none';
+}
+
+var wtf = document.querySelector('.dashboard-right');
+if(wtf != null && wtf != undefined) {
+    wtf.style.display = 'none';
+}
+
+var wtf = document.querySelector('.content-main');
+if(wtf != null && wtf != undefined) {
+    wtf.style.float = 'none';
+    wtf.style.margin = 'auto';
+}
+
+var wtf = document.querySelector('.WhoToFollow');
+if(wtf != null && wtf != undefined) {
+    wtf.style.display = 'none';
+}
+
+var wtf = document.querySelector('.Trends');
+if(wtf != null && wtf != undefined) {
+    wtf.style.display = 'none';
+}
+
+var wtf = document.querySelector('.Footer');
+if(wtf != null && wtf != undefined) {
+    wtf.style.display = 'none';
+}
