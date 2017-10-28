4c30f4b0c50f2ecc7ae213ed9428b917ed17b4c4
David Cavar <wizzard405@gmail.com>
2017-10-28 17:11:52 +0200

    Added options page Allow changing visibility per box Arbitraty timeline zoom options added Experimental img to url conversion option

--- content.js
+++ content.js
@@ -1,39 +1,101 @@
+var dl = dr = wtf = t = f = dpc = true;
+var citu = false;
+var ca = 'none';
+var tz = 0;
+
 function removeBoxes(mutations) {
-    var wtf = document.querySelector('.dashboard-left');
-    if(wtf != null && wtf != undefined) {
-        wtf.style.display = 'none';
+    var element = document.querySelector('.dashboard-left');
+    if(element != null && element != undefined && dl === true) {
+        element.style.display = 'none';
     }
 
-    var wtf = document.querySelector('.dashboard-right');
-    if(wtf != null && wtf != undefined) {
-        wtf.style.display = 'none';
+    var element = document.querySelector('.dashboard-right');
+    if(element != null && element != undefined && dr === true) {
+        element.style.display = 'none';
     }
 
-    var wtf = document.querySelector('.content-main');
-    if(wtf != null && wtf != undefined) {
-        wtf.style.float = 'none';
-        wtf.style.margin = 'auto';
+    var element = document.querySelector('.content-main');
+
+    if(element != null && element != undefined) {
+        if(ca !== 'none') {
+            if(ca === "center") {
+                element.style.float = 'none';
+                element.style.margin = 'auto';
+            } else {
+                element.style.float = ca;
+            }
+        }
+
+        element.style.zoom = 1 + parseFloat(tz);
     }
 
-    var wtf = document.querySelector('.WhoToFollow');
-    if(wtf != null && wtf != undefined) {
-        wtf.style.display = 'none';
+    var element = document.querySelector('.wtf-module');
+    if(element != null && element != undefined && wtf === true) {
+        element.style.display = 'none';
     }
 
-    var wtf = document.querySelector('.Trends');
-    if(wtf != null && wtf != undefined) {
-        wtf.style.display = 'none';
+    var element = document.querySelector('.Trends');
+    if(element != null && element != undefined && t === true) {
+        element.style.display = 'none';
     }
 
-    var wtf = document.querySelector('.Footer');
-    if(wtf != null && wtf != undefined) {
-        wtf.style.display = 'none';
+    var element = document.querySelector('.Footer');
+    if(element != null && element != undefined && f === true) {
+        element.style.display = 'none';
+    }
+
+    var element = document.querySelector('.DashboardProfileCard');
+    if(element != null && element != undefined && dpc === true) {
+        element.style.display = 'none';
+    }
+
+    if(citu === true) {
+        var media_containers = document.querySelectorAll('.AdaptiveMediaOuterContainer');
+
+        for(var i = 0; i < media_containers.length; i++) {
+            var images = media_containers[i].querySelectorAll('img');
+
+            if(images.length == 0) {
+                continue;
+            }
+
+            media_containers[i].innerHTML = '';
+            for(var j = 0; j < images.length; j++) {
+                media_containers[i].innerHTML += '<div style="padding-top: 5px"><a href="' + images[j].src + '" target="_blank">' + images[j].src + '</a></div>';
+            }
+        }
     }
 }
 
+function restoreOptions() {
+    chrome.storage.local.get({
+        dl: true,
+        dr: true,
+        ca: "none",
+        t: true,
+        wtf: true,
+        f: true,
+        dpc: true,
+        tz: 0,
+        citu: false
+    }, function(items) {
+        dl = items.dl;
+        dr = items.dr;
+        ca = items.ca;
+        t = items.t;
+        wtf = items.wtf;
+        f = items.f;
+        dpc = items.dpc;
+        tz = items.tz;
+        citu = items.citu;
+    });
+}
+
+restoreOptions();
 var observer = new MutationObserver(removeBoxes);
 var config = { attributes: false, childList: true, characterData: false, subtree: true};
 target = document.querySelector('body');
+
 if(target != null) {
     observer.observe(target, config);
 }
---
+++ options.js
@@ -0,0 +1,61 @@
+function saveOptions() {
+    var dashboard_left = document.getElementById('dashboard-left').checked;
+    var dashboard_right = document.getElementById('dashboard-right').checked;
+    var content_align = document.getElementById('content-align').value;
+    var trending = document.getElementById('trending').checked;
+    var who_to_follow = document.getElementById('who-to-follow').checked;
+    var footer = document.getElementById('footer').checked;
+    var dashboard_profile_card = document.getElementById('dashboard-profile-card').checked;
+    var timeline_zoom = document.querySelector('#timeline-zoom').value;
+    var convert_images_to_urls = document.querySelector('#convert-images-to-urls').checked;
+
+    chrome.storage.local.set({
+        dl: dashboard_left,
+        dr: dashboard_right,
+        ca: content_align,
+        t: trending,
+        wtf: who_to_follow,
+        f: footer,
+        dpc: dashboard_profile_card,
+        tz: timeline_zoom,
+        citu: convert_images_to_urls
+    }, function() {
+        var status = document.getElementById('status');
+        status.innerText = 'Saved.';
+    });
+}
+
+function restoreOptions() {
+    chrome.storage.local.get({
+        dl: true,
+        dr: true,
+        ca: "center",
+        t: true,
+        wtf: true,
+        f: true,
+        dpc: true,
+        tz: 0,
+        citu: false
+    }, function(items) {
+        document.getElementById('dashboard-left').checked = items.dl;
+        document.getElementById('content-align').value = items.ca;
+        document.getElementById('dashboard-right').checked = items.dr;
+        document.getElementById('trending').checked = items.t;
+        document.getElementById('who-to-follow').checked = items.wtf;
+        document.getElementById('footer').checked = items.f;
+        document.getElementById('dashboard-profile-card').checked = items.dpc;
+        document.querySelector('#timeline-zoom').value = items.tz;
+        document.querySelector('#timeline-zoom-value').innerText = (items.tz * 100) + '%';
+        document.querySelector('#convert-images-to-urls').checked = citu;
+    });
+}
+
+document.querySelector('#save-options-btn').addEventListener('click', function() {
+    saveOptions();
+});
+
+document.querySelector('#timeline-zoom').addEventListener('change', function(e) {
+    document.querySelector('#timeline-zoom-value').innerText = (e.target.value * 100) + '%';
+});
+
+restoreOptions();
