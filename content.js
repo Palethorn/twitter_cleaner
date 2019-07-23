9d7e094dc6f8855f3e28ba27e8b6da89e5d51fd5
David Cavar <david.cavar@morescreens.net>
2019-07-23 12:50:38 +0200

    #7 New twitter version

--- content.js
+++ content.js
@@ -1,120 +1,13 @@
 var enabled = true;
-var dl = dr = wtf = t = f = dpc = true;
-var citu = remove_suggestions = pt = dismissible = recap = false;
-var ca = 'center';
-var tz = 0;
+var selectors = [];
 
 function removeBoxes(mutations) {
-    var element = document.querySelector('.dashboard-left');
-
-    if(element != null && element != undefined && dl === true) {
-        element.style.display = 'none';
+    for(var i = 0; i < selectors.length; i++) {
+        var node = document.querySelector(selectors[i]);
+        node.parentNode.removeChild(node);
     }
 
-    var element = document.querySelector('.dashboard-right');
-
-    if(element != null && element != undefined && dr === true) {
-        element.style.display = 'none';
-    }
-
-    var element = document.querySelector('.content-main');
-
-    if(element != null && element != undefined) {
-        if(ca !== 'none') {
-            if(ca === "center") {
-                element.style.float = 'none';
-                element.style.margin = 'auto';
-            } else {
-                element.style.float = ca;
-            }
-        }
-
-        element.style.zoom = 1 + parseFloat(tz);
-    }
-
-    var element = document.querySelector('.wtf-module');
-
-    if(element != null && element != undefined && wtf === true) {
-        element.style.display = 'none';
-    }
-
-    var element = document.querySelector('.Trends');
-
-    if(element != null && element != undefined && t === true) {
-        element.style.display = 'none';
-    }
-
-    var element = document.querySelector('.Footer');
-
-    if(element != null && element != undefined && f === true) {
-        element.style.display = 'none';
-    }
-
-    var element = document.querySelector('.DashboardProfileCard');
-
-    if(element != null && element != undefined && dpc === true) {
-        element.style.display = 'none';
-    }
-
-    if(citu === true) {
-        var media_containers = document.querySelectorAll('.AdaptiveMediaOuterContainer');
-
-        for(var i = 0; i < media_containers.length; i++) {
-            var images = media_containers[i].querySelectorAll('img');
-
-            if(images.length == 0) {
-                continue;
-            }
-
-            media_containers[i].innerHTML = '';
-
-            for(var j = 0; j < images.length; j++) {
-                var div = document.createElement('div');
-                div.style.paddingTop = '5px';
-                var a = document.createElement('a');
-                a.href = images[j].src;
-                a.target = "_blank";
-                div.appendChild(a);
-                media_containers[i].appendChild(div);
-            }
-        }
-    }
-
-
-    if(remove_suggestions == true) {
-        var suggestions = document.querySelectorAll('[data-component-context=suggest_activity_tweet]');
-        
-        for(var i = 0; i < suggestions.length; i++) {
-            var li = suggestions[i].parentNode;
-            var parent = li.parentNode;
-            parent.removeChild(li);
-        }
-    }
-
-    if(pt == true) {
-        var promoted_tweets = document.querySelectorAll('.promoted-tweet');
-
-        for(var i = 0; i < promoted_tweets.length; i++) {
-            var li = promoted_tweets[i].parentNode;
-            li.parentNode.removeChild(li);
-        }
-    }
-
-    if(dismissible == true) {
-        var items = document.querySelectorAll('.DismissibleModule');
-        console.log('Removing dismissible', items.length);
-        for(var i = 0; i < items.length; i++) {
-            items[i].parentNode.removeChild[items[i]];
-        }
-    }
-
-    if(recap == true) {
-        var items = document.querySelectorAll('.has-recap');
-        console.log('Removing recap', items.length);
-        for(var i = 0; i < items.length; i++) {
-            items[i].parentNode.removeChild[items[i]];
-        }
-    }
+    document.querySelector('header').style.alignItems = 'flex-start';
 }
 
 function observe() {
@@ -132,35 +25,10 @@ function observe() {
 
 function restoreOptions() {
     chrome.storage.local.get({
-        dl: true,
-        dr: true,
-        ca: "center",
-        t: true,
-        wtf: true,
-        f: true,
-        dpc: true,
-        tz: 0,
-        citu: false,
-        remove_suggestions: false,
-        pt: false,
-        dismissible: false,
-        recap: false,
-        enabled: true
+        selectors: []
     }, function(items) {
-        dl = items.dl;
-        dr = items.dr;
-        ca = items.ca;
-        t = items.t;
-        wtf = items.wtf;
-        f = items.f;
-        dpc = items.dpc;
-        tz = items.tz;
-        citu = items.citu;
-        remove_suggestions = items.remove_suggestions;
-        pt = items.pt;
-        dismissible = items.dismissible;
-        recap = items.recap;
-        enabled = items.enabled;
+        console.log(items);
+        selectors = items.selectors;
 
         if(enabled) {
             observe();
--- options.js
+++ options.js
@@ -1,32 +1,17 @@
+var selectors = [];
+
 function saveOptions() {
-    var dashboard_left = document.getElementById('dashboard-left').checked;
-    var dashboard_right = document.getElementById('dashboard-right').checked;
-    var content_align = document.getElementById('content-align').value;
-    var trending = document.getElementById('trending').checked;
-    var who_to_follow = document.getElementById('who-to-follow').checked;
-    var footer = document.getElementById('footer').checked;
-    var dashboard_profile_card = document.getElementById('dashboard-profile-card').checked;
-    var timeline_zoom = document.querySelector('#timeline-zoom').value;
-    var convert_images_to_urls = document.querySelector('#convert-images-to-urls').checked;
-    var remove_suggestions = document.querySelector('#remove-suggestions').checked;
-    var remove_promoted_tweets = document.querySelector('#remove-promoted-tweets').checked;
-    var remove_dismissible_boxes = document.querySelector('#remove-dismissible-boxes').checked;
-    var remove_recap_boxes = document.querySelector('#remove-recap-boxes').checked;
+    selectors = [];
+    var elements = document.querySelectorAll('.selector');
+
+    for(var i = 0; i < elements.length; i++) {
+        selectors.push(elements[i].value);
+    }
+
+    console.log(selectors);
 
     chrome.storage.local.set({
-        dl: dashboard_left,
-        dr: dashboard_right,
-        ca: content_align,
-        t: trending,
-        wtf: who_to_follow,
-        f: footer,
-        dpc: dashboard_profile_card,
-        tz: timeline_zoom,
-        citu: convert_images_to_urls,
-        remove_suggestions: remove_suggestions,
-        pt: remove_promoted_tweets,
-        dismissible: remove_dismissible_boxes,
-        recap: remove_recap_boxes
+        selectors: selectors
     }, function() {
         var status = document.getElementById('status');
         status.innerText = 'Saved.';
@@ -35,43 +20,14 @@ function saveOptions() {
 
 function restoreOptions() {
     chrome.storage.local.get({
-        dl: true,
-        dr: true,
-        ca: "center",
-        t: true,
-        wtf: true,
-        f: true,
-        dpc: true,
-        tz: 0,
-        citu: false,
-        remove_suggestions: false,
-        pt: false,
-        dismissible: false,
-        recap: false
+        selectors: []
     }, function(items) {
-        document.getElementById('dashboard-left').checked = items.dl;
-        document.getElementById('content-align').value = items.ca;
-        document.getElementById('dashboard-right').checked = items.dr;
-        document.getElementById('trending').checked = items.t;
-        document.getElementById('who-to-follow').checked = items.wtf;
-        document.getElementById('footer').checked = items.f;
-        document.getElementById('dashboard-profile-card').checked = items.dpc;
-        document.querySelector('#timeline-zoom').value = items.tz;
-        document.querySelector('#timeline-zoom-value').innerText = (items.tz * 100) + '%';
-        document.querySelector('#convert-images-to-urls').checked = items.citu;
-        document.querySelector('#remove-suggestions').checked = items.remove_suggestions;
-        document.querySelector('#remove-promoted-tweets').checked = items.pt;
-        document.querySelector('#remove-dismissible-boxes').checked = items.dismissible;
-        document.querySelector('#remove-recap-boxes').checked = items.recap;
+        selectors = items.selectors;
     });
 }
 
 document.querySelector('#save-options-btn').addEventListener('click', function() {
     saveOptions();
-});
-
-document.querySelector('#timeline-zoom').addEventListener('change', function(e) {
-    document.querySelector('#timeline-zoom-value').innerText = (e.target.value * 100) + '%';
 });
 
 restoreOptions();
