9532e9d09ebf5888c0e2ffe2ea7cf05f64c36d12
David Cavar <david.cavar@morescreens.net>
2019-11-21 09:08:51 +0100

    New twitter version, simple logic

--- content.js
+++ content.js
@@ -2,12 +2,27 @@ var enabled = true;
 var selectors = [];
 
 function removeBoxes(mutations) {
-    for(var i = 0; i < selectors.length; i++) {
-        var node = document.querySelector(selectors[i]);
-        node.parentNode.removeChild(node);
+    var nodes = document.getElementsByClassName('css-1dbjc4n r-1uaug3w r-1uhd6vh r-t23y2h r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x');
+    
+    for(var i = 0; i < nodes.length; i++) {
+        nodes[i].parentNode.removeChild(nodes[i]);
     }
 
-    document.querySelector('header').style.alignItems = 'flex-start';
+    nodes = document.getElementsByClassName('css-1dbjc4n r-1niwhzg r-18bvks7 r-t23y2h r-1phboty r-1yadl64 r-ku1wi2 r-1udh08x');
+    
+    for(var i = 0; i < nodes.length; i++) {
+        nodes[i].parentNode.removeChild(nodes[i]);
+    }
+
+    nodes = document.getElementsByTagName('header');
+    console.log(nodes);
+    
+    for(var i = 0; i < nodes.length; i++) {
+        nodes[i].style.alignItems = 'initial';
+    }
+
+    document.querySelector('header').style = '';
+    document.querySelector('header').style.alignItems = '';
 }
 
 function observe() {
